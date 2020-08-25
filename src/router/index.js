f62e66579447028d949299ace066e14f8944a310
Sequona Hastings <slmhastings@gmail.com>
2020-08-25 18:08:46 -0500

    create show

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,33 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Logout from '../views/Logout.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Logout from "../views/Logout.vue";
+import Index from "../views/Index.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/logout',
-    name: 'Logout',
-    component: Logout
-  }
-]
+    path: "/logout",
+    name: "Logout",
+    component: Logout,
+  },
+  {
+    path: "/index",
+    name: "index",
+    component: Index,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
